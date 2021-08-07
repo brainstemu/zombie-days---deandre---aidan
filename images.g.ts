2943575fb661eebdf907118e8d0dd523d52fd128
BrainSTEM University <77249266+brainstemu@users.noreply.github.com>
2021-08-07 11:46:06 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,83 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "Kacnn8Qs9Q]xpCbl}oR]":
+            case "THE END OF THE ROAD":return img`
+f f f f f f f f 2 f f 2 f 2 2 f 
+f f f 2 2 2 f 2 f f f 2 f f f 2 
+2 2 f 2 2 2 2 f 2 f f 2 f f f 2 
+2 2 f 2 2 2 2 2 2 2 2 2 2 f f 2 
+f f f f 2 2 2 2 2 2 2 2 2 f f 2 
+f f f 2 2 2 2 2 2 2 2 2 f f f 2 
+f 2 2 f f f f f 2 2 2 f 2 2 f 2 
+f 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+f f 2 2 2 2 2 2 2 f f f 2 2 2 2 
+f 2 2 f f 2 2 2 2 2 2 2 2 2 2 2 
+f f 2 2 2 2 2 f f f f f f f f 2 
+2 2 2 f f 2 2 2 2 f f f 2 f f 2 
+2 2 2 2 2 2 2 2 2 f f 2 2 f f f 
+f f f 2 2 2 2 2 2 f 2 2 2 f f f 
+2 f f f f f f f f f f f 2 f f f 
+f 2 2 f 2 2 2 2 2 f 2 f f f f 2 
+`;
+            case "L6,vW_d@BIwK_$;r7BbK":
+            case "beta henchman":return img`
+........................
+........................
+........................
+........................
+..........ffff..........
+........ff5555ff........
+.......fb555555bf.......
+.......f55555555f.......
+......fd55555555df......
+......fd55555555df......
+......fd44555544df......
+......f44bfddfb44f......
+......f4dc8559cd4f......
+.......fb555555bf.......
+......fffcdb5bdffff.....
+....fc555cbfbfc555cf....
+....f5b5b5ffff5b5b5f....
+....fbfbffffffbfbfbf....
+.........ffffff.........
+...........fff..........
+........................
+........................
+........................
+........................
+`;
+            case "U7$lV_[@h~kx|/48q98B":
+            case "chris":return img`
+. . . . . . . . . . . . 
+. . . e e e e e e . . . 
+. e e e e e e e e e e . 
+. e e e e e e e e e e . 
+. e e 4 4 4 4 4 4 e e . 
+. 4 4 4 4 4 4 4 4 4 4 . 
+. 4 1 f 4 4 4 f 1 4 4 . 
+. 4 1 9 4 4 4 9 1 4 4 . 
+. 4 4 4 4 4 4 4 4 4 4 . 
+. . 4 4 4 f f 4 4 4 . . 
+. . . 4 4 4 4 4 4 . . . 
+. 4 4 b 1 1 1 1 b 4 4 . 
+. 4 4 1 1 1 1 1 1 4 4 . 
+. 5 5 6 6 6 6 6 6 5 5 . 
+. 2 2 f f f f f f 2 2 . 
+. . . f f . . f f . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
---
+++ tilemap.g.ts
@@ -0,0 +1,82 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`10001000150303010101010103030303031f1e0c1404010124060607010122010101010214040d1a0e1a10040101040101010102140411010e0114050606080606010102140411010101140d0e100415131313211404110101150f11011404140101011114041101150f0412130f04140101011114041101140908060606041401010111140412130f040d0e0e100414010101111405060701041101011404140101011114090608060a110101140414010101111404161816041213130f041b0e0e0e2014041716160506060606231d1d1d1d1d14041619160906071d1d1d1d1c1c1c1d14050606060a0b05071c1c1c1c1c1c1d1b0e0e0e0e050606231d1d1d1d1d1d1d`, img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 . . . 
+2 . 2 2 . . . . 2 2 . 2 2 2 2 2 
+2 . 2 2 2 2 2 . 2 2 . 2 2 2 2 2 
+2 . 2 2 2 2 2 . . . . . . 2 2 2 
+2 . 2 2 2 2 2 2 2 2 . 2 2 2 2 2 
+2 . 2 2 2 2 2 2 2 2 . 2 2 2 2 2 
+2 . 2 2 2 2 . 2 2 2 . 2 2 2 2 2 
+2 . 2 2 2 . . . . . . 2 2 2 2 2 
+2 . 2 2 2 . 2 2 2 2 . 2 2 2 2 2 
+2 . . . 2 . 2 . . 2 . 2 2 2 2 2 
+2 . . . . . 2 . . 2 . 2 2 2 2 2 
+2 . 2 2 2 . 2 2 2 2 . 2 2 2 2 2 
+2 . 2 2 2 . . . . . . 2 2 2 2 2 
+2 . 2 . 2 . . . 2 2 2 2 . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 2 2 2 2 . . . . 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,sprites.castle.tilePath5,sprites.castle.tilePath6,sprites.castle.tilePath2,sprites.vehicle.roadVertical,sprites.vehicle.roadTurn3,sprites.vehicle.roadHorizontal,sprites.vehicle.roadTurn2,sprites.vehicle.roadIntersection1,sprites.vehicle.roadTurn1,sprites.vehicle.roadIntersection4,sprites.dungeon.stairNorth,sprites.builtin.forestTiles3,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterWest1,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterSouth0,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenInnerNorthWest,sprites.dungeon.floorDark0,sprites.dungeon.floorDark1,sprites.dungeon.floorDark5,sprites.dungeon.doorClosedNorth,sprites.dungeon.greenOuterNorth1,sprites.dungeon.greenInnerSouthWest,sprites.dungeon.hazardWater,sprites.builtin.oceanDepths0,sprites.builtin.forestTiles2,sprites.builtin.forestTiles1,sprites.dungeon.greenInnerSouthEast,sprites.dungeon.greenInnerNorthEast,sprites.dungeon.stairLadder,myTiles.tile3,myTiles.tile1], TileScale.Sixteen);
+            case "the lair":
+            case "level2":return tiles.createTilemap(hex`1000100002020202020202020203020202020202020202020202020202030202020202020202020202030303030302020202020202010101010301010101010101020202020103030303030303030301010202020101030303030303030303030101020201030303030303030303030303010202010303030303030303030303030101020101030303030303030303030303010202010303030303040303030303030102020103030303030303030303030101020201030303030303030303030301020202010303030303030303030303010202020101030303030303030303010102020202010103030303030303030102020202020201010101010101010101020202`, img`
+2 2 2 2 2 2 2 2 2 . 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 . 2 2 2 2 2 2 
+2 2 2 2 2 . . . . . 2 2 2 2 2 2 
+2 2 2 2 2 . 2 2 2 2 2 2 2 2 2 2 
+2 2 . . . . . . . . . 2 2 2 2 2 
+2 2 . . . . . . . . . . 2 2 2 2 
+2 . . . . . . . . . . . . 2 2 2 
+2 . . . . . . . . . . . . 2 2 2 
+2 2 . . . . . . . . . . . . 2 2 
+2 2 . . . . . . . . . . . . 2 2 
+2 2 . . . . . . . . . . . 2 2 2 
+2 2 . . . . . . . . . . . 2 2 2 
+2 2 . . . . . . . . . . . 2 2 2 
+2 2 2 . . . . . . . . . 2 2 2 2 
+2 2 2 2 . . . . . . . . 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,myTiles.tile4,sprites.dungeon.floorDark0,sprites.dungeon.floorLight2,sprites.dungeon.hazardHole], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "myTile3":
+            case "tile5":return tile5;
+            case "myTile":
+            case "tile1":return tile1;
+            case "happy coins":
+            case "tile3":return tile3;
+            case "fire charger":
+            case "tile4":return tile4;
+            case "rage coins":
+            case "tile2":return tile2;
+            case "myTile0":
+            case "tile6":return tile6;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
